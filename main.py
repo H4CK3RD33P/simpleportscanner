0b125399314d46f9714e5da266225dcee91b6c24
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-05-22 23:31:54 +0530

    Added code to fetch IP Address
    User either must provide the public/private IP of the target or let the program get it by itself, in case
    of which the user must provide a valid FQDN (https://www.google.com)

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import socket
 import sys
 import datetime
 
+#GETTING THE IP
+
+choice = input("Do you have the target IP? ")
+if ("y" in choice.lower()):
+	target_ip = input("Enter IP address (IPv4): ")
+elif ("n" in choice.lower()):
+	target_address = input("Enter target address: ")
+	target_ip = socket.gethostbyname(target_address)
+else:
+	print("IP or address is a must :( ")
+	sys.exit()
+
