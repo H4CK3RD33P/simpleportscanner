41c399e1ddca64a24fd559b08e1d8e8b535f64c9
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-05-22 23:57:31 +0530

    Added termcolor module
    Added colors and minimalistic VFX to all the plain texts

--- main.py
+++ main.py
@@ -3,37 +3,38 @@
 import socket
 import sys
 import datetime
+import termcolor
 
 #GETTING THE IP
 
-choice = input("Do you have the target IP? ")
+choice = input(termcolor.colored("Do you have the target IP? ",color='green'))
 if ("y" in choice.lower()):
-	target_ip = input("Enter IP address (IPv4): ")
+	target_ip = input(termcolor.colored("Enter IP address (IPv4): ",color="magenta"))
 elif ("n" in choice.lower()):
-	target_address = input("Enter target address: ")
+	target_address = input(termcolor.colored("Enter target address: ",color="yellow"))
 	target_ip = socket.gethostbyname(target_address)
 else:
-	print("IP or address is a must :( ")
+	print(termcolor.colored("IP or address is a must :( ",color='red'))
 	sys.exit()
 
 #OPEN PORT DETECTION
 
-print(">>"*50)
-print("Scanning target: {}".format(target_ip))
-print("Started At: {}".format(datetime.datetime.now()))
-print("<<"*50)
+print(termcolor.colored(">>"*50,color="red",attrs=['blink']))
+print(termcolor.colored("Scanning target: {}".format(target_ip),color='red',attrs=['reverse']))
+print(termcolor.colored("Started At: {}".format(datetime.datetime.now()),color='green',attrs=['underline','bold']))
+print(termcolor.colored("<<"*50,color='red',attrs=["blink"]))
 try:
 	for port in range(1,1024):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
 		result = s.connect_ex((target_ip,port))
 		if (result == 0):
-			print("Port {} is open".format(port))
+			print(termcolor.colored("Port {} is open".format(port),color='blue',attrs=['bold']))
 		s.close()
 except KeyboardInterrupt:
-	print("Closing the scanner")
+	print(termcolor.colored("Closing the scanner",color='magenta'))
 	sys.exit()
 except socket.gaierror:
-	print("unable to establish connection")
-print("-"*50)
-print("Scanning finished at {}".format(datetime.datetime.now()))
+	print(termcolor.colored("unable to establish connection",color='red',attrs=['reverse','blink']))
+print(termcolor.colored("-"*50,color='red',attrs=['red']))
+print(termcolor.colored("Finished At: {}".format(datetime.datetime.now()),color='green',attrs=['underline','bold']))
